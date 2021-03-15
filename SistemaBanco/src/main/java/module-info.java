ffc981af498ad020510f9ff815763f2d8e8218f7
Ellen <52865288+Spadotto@users.noreply.github.com>
2021-03-15 16:49:54 -0300

    Delete SistemaBanco directory

--- SistemaBanco/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module poo2.SistemaBanco {
-    requires transitive javafx.controls;
-    requires transitive javafx.fxml;
-	requires transitive javafx.graphics;
-	requires javafx.base;
-
-    opens poo2.SistemaBanco to javafx.fxml;
-    exports poo2.SistemaBanco;
-}
